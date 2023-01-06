2aef7d613e9bf9c2f99ebca1ebf986bb49bb28a2
aibrahim@hsk-partners.com <ahmadji.ibr11@gmail.com>
2023-01-06 04:31:30 +0100

    remove unnecessary things

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,6 @@ import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 import {ValidationPipe} from "@nestjs/common";
 import {UsersSeedService} from "./users/users-seed.service";
-import {User} from "./users/entities/user.entity";
-import * as bcrypt from "bcrypt";
-import {Role} from "./users/user-role.enum";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
